4092a6568d655614a1f5281e47f3fa9feb01810c
Safeer Hussain <bcsm-s20-033@superior.edu.pk>
2023-11-15 23:50:01 +0500

    simple calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: dead_code
+
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -7,67 +9,100 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
-  Future<List> fetchData() async {
-    var url = "https://mocki.io/v1/79c4fbfe-5de6-4628-8b88-a585f5608c59";
-    var uri = Uri.parse(url);
-    var res = await http.get(uri);
-    return jsonDecode(res.body);
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  var firstValue = TextEditingController();
+  var secondValue = TextEditingController();
+  var finalValue = TextEditingController();
+
+  dynamic calculation(
+      {required dynamic value1,
+      required dynamic value2,
+      required String operator}) {
+    switch (operator) {
+      case "+":
+        return (int.parse(value1.text) + int.parse(value2.text)).toString();
+        break;
+      case "-":
+        return (int.parse(value1.text) - int.parse(value2.text)).toString();
+        break;
+      case "*":
+        return (int.parse(value1.text) * int.parse(value2.text)).toString();
+        break;
+      case "/":
+        return (int.parse(value1.text) / int.parse(value2.text)).toString();
+        break;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text("HTTP")),
-        body: FutureBuilder(
-          future: fetchData(),
-          builder: (context, snapshot) {
-            return snapshot.hasData
-                ? ListView.builder(
-                    itemBuilder: (context, i) => Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(9.0),
-                          color: Colors.amber,
-                          boxShadow: const [
-                            BoxShadow(color: Colors.black, blurRadius: 5.0),
-                          ]),
-
-                      alignment: Alignment.center,
-                      margin: const EdgeInsets.all(9.0),
-                      // color: Colors.amber,
-                      child: Text("${snapshot.data?[i]["id"]}"),
-                    ),
-                    itemCount: snapshot.data?.length,
-                  )
-                : const Center(
-                    child: CircularProgressIndicator(),
-                  );
-          },
+        appBar: AppBar(title: const Text("Calculator")),
+        body: Container(
+          padding: const EdgeInsets.all(15.0),
+          color: Colors.blue.shade100,
+          child: Column(children: [
+            TextField(
+              controller: firstValue,
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              controller: secondValue,
+              keyboardType: TextInputType.number,
+            ),
+            TextField(controller: finalValue, enabled: false),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      finalValue.text = calculation(
+                          value1: firstValue,
+                          value2: secondValue,
+                          operator: "+");
+                      setState(() {});
+                    },
+                    child: const Text("ADD")),
+                ElevatedButton(
+                    onPressed: () {
+                      finalValue.text = calculation(
+                          value1: firstValue,
+                          value2: secondValue,
+                          operator: "-");
+                      setState(() {});
+                    },
+                    child: const Text("SUB")),
+                ElevatedButton(
+                    onPressed: () {
+                      finalValue.text = calculation(
+                          value1: firstValue,
+                          value2: secondValue,
+                          operator: "/");
+                      setState(() {});
+                    },
+                    child: const Text("DIV")),
+                ElevatedButton(
+                    onPressed: () {
+                      finalValue.text = calculation(
+                          value1: firstValue,
+                          value2: secondValue,
+                          operator: "*");
+                      setState(() {});
+                    },
+                    child: const Text("MUL")),
+              ],
+            )
+          ]),
         ),
       ),
     );
   }
 }
-
-/*
-data != null
-            ? ListView.builder(
-                itemBuilder: (context, i) => Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(9.0),
-                      color: Colors.amber,
-                      boxShadow: const [
-                        BoxShadow(color: Colors.black, blurRadius: 5.0),
-                      ]),
-
-                  alignment: Alignment.center,
-                  margin: const EdgeInsets.all(9.0),
-                  // color: Colors.amber,
-                  child: Text("${data?[i]["id"]}"),
-                ),
-                itemCount: data?.length,
-              )
-*/
